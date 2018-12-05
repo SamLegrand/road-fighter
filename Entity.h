bdcd5dd51f2bce21a908ae5b5e63396c0081a0d7
Sam Legrand <sam.legrand@student.uantwerpen.be>
2018-12-05 20:49:00 +0100

    Moved source files to proper src folder

---
+++ Entity.h
@@ -0,0 +1,16 @@
+//
+// Created by sam on 05/12/18.
+//
+
+#ifndef ROAD_FIGHTER_ENTITY_H
+#define ROAD_FIGHTER_ENTITY_H
+
+
+
+class Entity {
+
+};
+
+
+
+#endif //ROAD_FIGHTER_ENTITY_H
---
+++ PlayerCar.h
@@ -0,0 +1,16 @@
+//
+// Created by sam on 05/12/18.
+//
+
+#ifndef ROAD_FIGHTER_PLAYERCAR_H
+#define ROAD_FIGHTER_PLAYERCAR_H
+
+
+
+class PlayerCar {
+
+};
+
+
+
+#endif //ROAD_FIGHTER_PLAYERCAR_H
