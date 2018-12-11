0731b59a826413a69a89814daafce78a8ba1c1d5
Sam <sam.legrand@student.uantwerpen.be>
2018-12-11 20:02:46 +0100

    Added Game class

---
+++ src/road_fighter/Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sam on 11.12.18.
+//
+
+#include "Game.h"
---
+++ src/road_fighter/Game.h
@@ -0,0 +1,14 @@
+//
+// Created by sam on 11.12.18.
+//
+
+#ifndef ROAD_FIGHTER_GAME_H
+#define ROAD_FIGHTER_GAME_H
+
+namespace road_fighter {
+    class Game {
+
+    };
+}
+
+#endif //ROAD_FIGHTER_GAME_H
