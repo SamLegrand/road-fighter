e16ef48b32da492c18b5cba573dda437bde15c99
Sam <sam.legrand@student.uantwerpen.be>
2018-12-25 19:14:34 +0100

    Added main game loop

--- src/road_fighter/main.cpp
+++ src/road_fighter/main.cpp
@@ -1,24 +1,80 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <chrono>
+#include <zconf.h>
+#include "Game.h"
+#include "../road_fighter_sfml/EntityFactorySFML.h"
+#include <time.h>
+#include <thread>
+
+using namespace std;
+using namespace chrono;
+using namespace road_fighter_SFML;
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    shared_ptr<sf::RenderWindow> window = make_shared<sf::RenderWindow>(sf::VideoMode(1280, 960, desktop.bitsPerPixel), "Road Fighter");
+//    sf::RenderWindow window(sf::VideoMode(desktop.width, desktop.height, desktop.bitsPerPixel), "SFML works!", sf::Style::Fullscreen);
 
-    while (window.isOpen())
+    shared_ptr<road_fighter::EntityFactory> e = make_shared<EntityFactorySFML>(window);
+    road_fighter::Transformation::getInstance().setResolution(1280, 960);
+    road_fighter::Game g(e);
+
+//    sf::CircleShape shape(100.f);
+//    shape.setFillColor(sf::Color::Green);
+
+    steady_clock::time_point fpsTimer(steady_clock::now());
+    duration<int32_t, ratio<1, 60>> FPS{};
+    window->setVerticalSyncEnabled(false);
+//    microseconds loopTime{};
+
+//    sf::Texture bgTexture;
+//    bgTexture.loadFromFile("../graphics/Background.png");
+//    sf::Sprite background;
+//    background.setTexture(bgTexture);
+//    sf::Sprite background2 = background;
+//    background2.move(0, -960);
+//    window->setView(view);
+//    sf::Vector2f oldSize = static_cast<sf::Vector2f>(background.getTexture()->getSize());
+//    sf::Vector2f newSize(static_cast<float>(1280), static_cast<float>(960));
+//    background.setScale(newSize.x/oldSize.x, newSize.y/oldSize.y);
+
+    while (window->isOpen())
     {
+//        if (background.getPosition().y >= 960) {
+//            background.setPosition(0, -960);
+//        }
+//        if (background2.getPosition().y >= 960) {
+//            background2.setPosition(0, -960);
+//        }
+//        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+//            background.move(0, 0.03f);
+//            background2.move(0, 0.03f);
+//        }
+
+//        FPS = duration_cast<duration<int32_t, ratio<1, 60>>>(steady_clock::now() - fpsTimer);
+//        if (FPS.count() >= 1)
+//        {
+        fpsTimer = steady_clock::now();
         sf::Event event;
-        while (window.pollEvent(event))
+        while (window->pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-                window.close();
+                window->close();
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape) {
+                    window->close();
+                }
+            }
         }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+        g.handleInput();
+        g.handleMovement();
+        g.spawnPassableCar();
+        window->clear();
+        g.drawEntities();
+        window->display();
+        this_thread::sleep_until(fpsTimer + duration_cast<microseconds>(duration<double>(1/60.0)));
+//        }
     }
-
     return 0;
 }
